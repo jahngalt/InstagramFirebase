983f965794ed8772703c150a569e781d39844731
Oleg Kudimov <ovkudimov@gmail.com>
2020-03-23 13:09:24 +0300

    Initial Commit

---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by Oleg Kudimov on 3/23/20.
+//  Copyright © 2020 Oleg Kudimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
